e2a743d50d3e2e795a3965152853be98ab9db28a
Otavio Cesario <otaviocesariostos@gmail.com>
2022-11-11 01:25:53 -0300

    Commit Listas Variaveis

--- cypress/e2e/pre-cadastro.spec.cy.js
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -8,13 +8,17 @@ describe('Funcionalidade Pre Cadastro', () => {
     })
 
     it('Deve completar o pre cadastro com sucesso ', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        let emailFaker = faker.internet.email(nomeFaker)
+        let nomeFaker = faker.name.firstName() 
+        let sobrenomeFaker = faker.name.lastName()
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@teste.com')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.page-title').should('contain' , 'Minha conta')
