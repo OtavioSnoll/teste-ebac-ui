3d8d6705c55a672971a84483b57d55ea962f3945
Otavio Cesario <otaviocesariostos@gmail.com>
2022-11-16 18:13:50 -0300

    commit lista de massa entrega

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -6,38 +6,56 @@ const dadosEndereco = require('../fixtures/endereco.json')
 describe('Funicionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados => { 
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
+
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        EnderecoPage.editarEnderecoFaturamento('Flavio', 'Araujo', 'Google', 'Brasil', 'Rua Pauliceia','251', 'Osasco', 'São Paulo','06243240','11973335598', 'otaviocesario@hotmail.com' )
+        EnderecoPage.editarEnderecoFaturamento('Flavio', 'Araujo', 'Google', 'Brasil', 'Rua Pauliceia', '251', 'Osasco', 'São Paulo', '06243240', '11973335598', 'otaviocesario@hotmail.com')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
 
 
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
         EnderecoPage.editarEnderecoFaturamento(
-            dadosEndereco [1].nome,
-            dadosEndereco [1].sobrenome,
-            dadosEndereco [1].empresa,
-            dadosEndereco [1].pais,
-            dadosEndereco [1].endereco,
-            dadosEndereco [1].numero,
-            dadosEndereco [1].cidade,
-            dadosEndereco [1].estado,
-            dadosEndereco [1].cep,
-            dadosEndereco [1].telefone,
-            dadosEndereco [1].email
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+        )
+        });
+
+    it('Deve fazer cadastro de entrega com sucesso', () => {
+        EnderecoPage.editarEnderecoEntrega('Flavio', 'Araujo', 'Google', 'Brasil', 'Rua Pauliceia', '251', 'Osasco', 'São Paulo', '06243240', '11973335598', 'otaviocesario@hotmail.com')
+
+    });
+
+        it.only('Deve fazer cadastro de entrega com sucesso - usando arquivo de dados', () => {
+            EnderecoPage.editarEnderecoEntrega(
+                dadosEndereco[1].nome,
+                dadosEndereco[1].sobrenome,
+                dadosEndereco[1].empresa,
+                dadosEndereco[1].pais,
+                dadosEndereco[1].endereco,
+                dadosEndereco[1].numero,
+                dadosEndereco[1].cidade,
+                dadosEndereco[1].estado,
+                dadosEndereco[1].cep,
+            
+            
+            
             )
 
-
-        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
-
+        });
 
     });
-
-});
